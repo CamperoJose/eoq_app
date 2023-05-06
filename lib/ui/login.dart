f8959604c5c54a55e294ed95bd4be49c2c4e04e5
CamperoJose <jose.campero@ucb.edu.bo>
2023-05-05 21:52:53 -0400

    login

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../Componentes/button1.dart';
 import '../Componentes/text_field1.dart';
+import 'calculator.dart';
 
 class LoginView extends StatelessWidget {
   final TextEditingController _userController = TextEditingController();
@@ -23,77 +24,88 @@ class LoginView extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: screenWidth > 600 // pantalla más grande que un iPad horizontal
-                ? Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Image.asset(
-                          'assets/logo01.png',
-                          width: 400,
-                          height: 400,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextInput1(
-                              controller: _userController,
-                              hint: "Ingrese su usuario",
-                              obscureText: false,
+            child:
+                screenWidth > 600 // pantalla más grande que un iPad horizontal
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Image.asset(
+                              'assets/logo01.png',
+                              width: 400,
+                              height: 400,
                             ),
-                            const SizedBox(height: 20),
-                            TextInput1(
-                              controller: _passwordController,
-                              hint: "Ingrese su contraseña",
-                              obscureText: true,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextInput1(
+                                  controller: _userController,
+                                  hint: "Ingrese su usuario",
+                                  obscureText: false,
+                                ),
+                                const SizedBox(height: 20),
+                                TextInput1(
+                                  controller: _passwordController,
+                                  hint: "Ingrese su contraseña",
+                                  obscureText: true,
+                                ),
+                                const SizedBox(height: 20),
+                                CustomElevatedButton(
+                                  buttonText: "Ingresar",
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                CalculatorView()));
+                                  },
+                                ),
+                                const SizedBox(
+                                    height:
+                                        20), // Añadir espacio entre botón y texto
+                              ],
                             ),
-                            const SizedBox(height: 20),
-                            CustomElevatedButton(
-                              buttonText: "Ingresar",
-                              onPressed: () {
-                                print("redirige al login");
-                              },
-                            ),
-                            const SizedBox(height: 20), // Añadir espacio entre botón y texto
-                          ],
-                        ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/logo01.png',
+                            width: 200,
+                            height: 200,
+                          ),
+                          const SizedBox(height: 20),
+                          TextInput1(
+                            controller: _userController,
+                            hint: "Ingrese su usuario",
+                            obscureText: false,
+                          ),
+                          const SizedBox(height: 20),
+                          TextInput1(
+                            controller: _passwordController,
+                            hint: "Ingrese su contraseña",
+                            obscureText: true,
+                          ),
+                          const SizedBox(height: 20),
+                          CustomElevatedButton(
+                            buttonText: "Ingresar",
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => CalculatorView()));
+                            },
+                          ),
+                          const SizedBox(
+                              height: 20), // Añadir espacio entre botón y texto
+                        ],
                       ),
-                    ],
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/logo01.png',
-                        width: 200,
-                        height: 200,
-                      ),
-                      const SizedBox(height: 20),
-                      TextInput1(
-                        controller: _userController,
-                        hint: "Ingrese su usuario",
-                        obscureText: false,
-                      ),
-                      const SizedBox(height: 20),
-                      TextInput1(
-                        controller: _passwordController,
-                        hint: "Ingrese su contraseña",
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 20),
-                      CustomElevatedButton(
-                        buttonText: "Ingresar",
-                        onPressed: () {
-                          print("redirige al login");
-                        },
-                      ),
-                      const SizedBox(height: 20), // Añadir espacio entre botón y texto
-                    ],
-                  ),
           ),
         ),
       ),
